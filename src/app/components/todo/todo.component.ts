f03c4a5828ef904799e7cef4e0f882fdb65dec25
Nyerges Dominik <dommi7797@gmail.com>
2021-03-21 14:18:46 +0100

    created todo model

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from './../../models/Todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todos:Todo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First Todo',
+        completed: false,
+      }
+      {
+        content: 'Second Todo',
+        completed: true,
+      }
+    ]
+  }
+
+}
